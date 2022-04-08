aa957c1ce4020d3ae9c849f6cd7ab10a62142d9f
Nestor <77801432+Nestor-B@users.noreply.github.com>
2022-04-07 23:43:15 -0400

    Add files via upload

---
+++ Curriculum/js/main.js
@@ -0,0 +1,49 @@
+let button = document.querySelectorAll('button')
+let imgPerfil = document.querySelector('#imgPerfil')
+let contentExperience = document.querySelector('#contentExperience')
+
+window.addEventListener('beforeprint', (event) => {
+    button.forEach((e)=>{
+        e.style.display = 'none'
+    })
+})
+
+document.addEventListener('mouseover', (e) => {
+    button.forEach((e)=>{
+        e.style.display = 'block'
+    })            
+})
+
+function ChangePhoto(e){
+    let file = e.files[0]
+    let RF = new FileReader()
+    RF.onload = function(e) {
+        imgPerfil.src = e.target.result
+    }
+    RF.readAsDataURL(file)
+}
+
+function addExperience(){
+    contentExperience.insertAdjacentHTML("beforebegin", `
+        <div>
+            <h3>New Title</h3>
+            <br>
+            <p>Job company name | Year</p>
+            <ul>
+                <li>Job</li>
+            </ul>
+        </div>            
+    `)
+}
+
+function addEducation(){
+    let contentEducation = document.querySelector('#contentEducation')
+    contentEducation.insertAdjacentHTML("afterend", `
+    <br>
+    <div>
+        <h3>Computer engineer</h3>
+        <br>
+        <p>Universidad privada en Santo Domingo | 2017 - 2022</p>
+        </div>            
+    `)
+}
